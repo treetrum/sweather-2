89bffc8605a110a3c7af54fd10073e988db9adaa
Sam Davis <sam@sjd.co>
2022-10-23 16:29:13 +1100

    Update version

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 
+SWEATHER_REPO_ROOT="$(dirname "$BASH_SOURCE")"/..
+CONFIG_FILE=$SWEATHER_REPO_ROOT/sweather-2/Build\ Config/config.xcconfig
+
 # Load .env from source if it exists
-if [ -f ${CI_WORKSPACE}/.env ]
+if [ -f ${SWEATHER_REPO_ROOT}/.env ]
 then
-    export $(grep -v '^#' $CI_WORKSPACE/.env | xargs)
+    export $(grep -v '^#' $SWEATHER_REPO_ROOT/.env | xargs)
 fi
 
 # Add env vars to config.xcconfig
-echo "WILLY_WEATHER_API_KEY = $WILLY_WEATHER_API_KEY" > $CI_WORKSPACE/sweather-2/Build\ Config/config.xcconfig
+echo "WILLY_WEATHER_API_KEY = $WILLY_WEATHER_API_KEY" > $CONFIG_FILE
 
 exit 0
